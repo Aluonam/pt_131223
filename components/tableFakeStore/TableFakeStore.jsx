3bb610dd425219ca639cb1d5104ed7cd1e1d5781
Paula Cano <paulacanoroman@gmail.com>
2023-12-13 20:18:14 +0100

    feat: api call with try catch

--- components/tableFakeStore/TableFakeStore.jsx
+++ components/tableFakeStore/TableFakeStore.jsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import TableFakeStoreStyle from './TableFakeStore.module.css'
 
 const TableFakeStore = () => {
 
     const [dataAPI, setDataAPI] = useState([])
 
+    useEffect(() => {
+        apiCall()
+    }, [])
+    
 
-    // https://fakestoreapi.com/products?limit=5
+    const apiCall = async ()=>{
+        try{
+            const url = await fetch(`https://fakestoreapi.com/products?limit=5`);
+            const data = await url.json();
+            console.log(data)
+        }catch(err){
+            console.log(err, "error detected")
+        }
+    }
 
   return (
     <>
