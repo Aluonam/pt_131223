5ca5973947e44409e2faf48445cbcf549e016ebd
Paula Cano <paulacanoroman@gmail.com>
2023-12-13 20:37:13 +0100

    feat: save value inputs in useState

--- components/form/Form.jsx
+++ components/form/Form.jsx
@@ -3,25 +3,18 @@ import style from './Form.module.css'
 
 const Form = () => {
 
-  const [dataUser, setdataUser] = useState([
-    {
-      number: '',
-      text:'',
-      password: '',
-      email: '',
-      checkbox: ''
-    }
-  ])
+  const [dataUser, setDataUser] = useState({})
 
 
+  console.log(dataUser)
   return (
     <>
     <form>
-        <input type='number' value={dataUser.number}></input>
-        <input type='text' value={dataUser.text}></input>
-        <input type='password' value={dataUser.password}></input>
-        <input type='email' value={dataUser.email}></input>
-        <input type='checkbox' value={dataUser.checkbox}></input>
+        <input type='number' value={dataUser?.number} onChange={(e)=>{setDataUser({...dataUser,number:e.target.value})}}></input>
+        <input type='text' value={dataUser?.text}  onChange={(e)=>{setDataUser({...dataUser,text:e.target.value})}}></input>
+        <input type='password' value={dataUser?.password}  onChange={(e)=>{setDataUser({...dataUser,password:e.target.value})}}></input>
+        <input type='email' value={dataUser?.email}  onChange={(e)=>{setDataUser({...dataUser,email:e.target.value})}}></input>
+        <input type='checkbox' value={dataUser?.checkbox}  onChange={(e)=>{setDataUser({...dataUser,checkbox:e.target.value})}}></input>
     </form>
     </>
   )
