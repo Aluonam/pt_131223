82d3c586a133e6adc2b0ce87c45d4ef2a5591392
Paula Cano <paulacanoroman@gmail.com>
2023-12-13 20:50:07 +0100

    feat: inputs save data in useState

--- components/deleteOneLetter/DeleteOneLetter.jsx
+++ components/deleteOneLetter/DeleteOneLetter.jsx
@@ -4,14 +4,16 @@ const DeleteOneLetter = () => {
 
     const [dataUser, setDataUser] = useState("")
     const [letterUser, setLetterUser] = useState("")
+
+    const deleteLetter = ()=>{
+
+    }
   return (
     <>
     <h3>Write a sentence</h3>
-    <input onChange={(e)=>{e.target.value}}></input>
-    <br>
-    </br>
+    <input onChange={(e)=>{setLetterUser(e.target.value)}}></input>
     <h3>Write a letter</h3>
-    <input onChange={(e)=>{e.target.value}} maxlength="1"></input>
+    <input onChange={(e)=>{setDataUser(e.target.value)}} maxlength="1"></input>
     <button onClick={()=>{}}>Click delete letter</button>
     </>
   )
