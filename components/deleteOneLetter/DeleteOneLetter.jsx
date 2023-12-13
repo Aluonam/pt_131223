0720c0628869dda85a590cc2a9843883490fea47
Paula Cano <paulacanoroman@gmail.com>
2023-12-13 20:56:41 +0100

    feat: split filter and join

--- components/deleteOneLetter/DeleteOneLetter.jsx
+++ components/deleteOneLetter/DeleteOneLetter.jsx
@@ -6,15 +6,19 @@ const DeleteOneLetter = () => {
     const [letterUser, setLetterUser] = useState("")
 
     const deleteLetter = ()=>{
-
+        const sentence = structuredClone(dataUser);
+        const letterByLetter = sentence.split("")
+        const senteceFiltered = letterByLetter.filter((letter)=>letter !== letterUser)
+        setDataUser(senteceFiltered.join(""))
     }
   return (
     <>
     <h3>Write a sentence</h3>
-    <input onChange={(e)=>{setLetterUser(e.target.value)}}></input>
+    <input onChange={(e)=>{setDataUser(e.target.value)}}></input>
     <h3>Write a letter</h3>
-    <input onChange={(e)=>{setDataUser(e.target.value)}} maxlength="1"></input>
-    <button onClick={()=>{}}>Click delete letter</button>
+    <input onChange={(e)=>{setLetterUser(e.target.value)}} maxlength="1"></input>
+    <button onClick={()=>{deleteLetter()}}>Click delete letter</button>
+    {dataUser}
     </>
   )
 }
