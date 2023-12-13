6df21fa6699c32c46719580c7f5928e9bb1c4f12
Paula Cano <paulacanoroman@gmail.com>
2023-12-13 20:43:43 +0100

    docs: checkbox

--- components/form/Form.jsx
+++ components/form/Form.jsx
@@ -15,7 +15,8 @@ const Form = () => {
         <input type='text' value={dataUser?.text}  onChange={(e)=>{setDataUser({...dataUser,text:e.target.value})}}></input>
         <input type='password' value={dataUser?.password}  onChange={(e)=>{setDataUser({...dataUser,password:e.target.value})}}></input>
         <input type='email' value={dataUser?.email}  onChange={(e)=>{setDataUser({...dataUser,email:e.target.value})}}></input>
-        <input type='checkbox' value={dataUser?.checkbox}  onChange={(e)=>{setDataUser({...dataUser,checkbox:e.target.value})}}></input>
+        <input type='checkbox' value={dataUser?.checkbox}  onChange={(e)=>{setDataUser({...dataUser,checkbox:e.target.value})}}></input> 
+        {/* arreglar funcionalidad checkbox con el e.target.value correcto */}
     </form>
     <ModalAnt dataUser={dataUser}></ModalAnt>
     </>
