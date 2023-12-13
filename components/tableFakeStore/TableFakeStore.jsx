ee96fc7ed98cb3b4d896b0f57f6d082faabc60f0
Paula Cano <paulacanoroman@gmail.com>
2023-12-13 20:15:48 +0100

    feat: useState and table

--- components/tableFakeStore/TableFakeStore.jsx
+++ components/tableFakeStore/TableFakeStore.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import TableFakeStoreStyle from './TableFakeStore.module.css'
 
 const TableFakeStore = () => {
+
+    const [dataAPI, setDataAPI] = useState([])
+
+
+    // https://fakestoreapi.com/products?limit=5
+
   return (
-    <div>TableFakeStore</div>
+    <>
+    <table>
+        <thead>
+            <tr>
+                <td>Product</td>
+                <td>Price</td>
+                <td>Category</td>
+                <td>Image</td>
+            </tr>
+        </thead>
+        <tbody>
+        <tr>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+                <td>1</td>
+            </tr>
+        </tbody>
+    </table>
+    </>
   )
 }
 
